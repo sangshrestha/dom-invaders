89521f07872cbe6c21c56bb05fd43cb8b2cb976f
sangshrestha <sang.socials@gmail.com>
2024-10-06 10:18:10 +0100

    feat: inject content script on extension click

--- background.js
+++ background.js
@@ -1,3 +1,8 @@
 chrome.action.onClicked.addListener(async (tab) => {
-  console.log("dom invaders ", tab);
+  console.log("dom-invaders background.js");
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ["content.js"],
+  });
 });
